479e44b3029e574ac9cb989d878d8807b6293495
EvoltPratom <bishantad@gmail.com>
2022-12-17 14:45:53 +0545

    Day6: using windows to search through a datastream to find window_size consecutive unique characters

--- day6/first.py
+++ day6/first.py
@@ -1,1 +1,25 @@
-first.p
+with open("day6input.txt") as f:
+    datastream = f.read()
+
+
+window_size = 14
+
+temp = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+for index,char in enumerate(datastream):
+
+    #store the first 4 characters from this index in a buffer variable
+    buffer = datastream[index:index+window_size]
+    # print(buffer)
+
+    #check if it has repeating values
+    if len(buffer) == len(set(buffer)):
+
+        #now the index + 4 is the answer
+        print(buffer)
+        print(index+window_size)
+        exit()
+
+
+'''
+For the first part, window_size = 4, and window_size = 14 for the second part
+'''
